00b0188e4fd87e0638e34d82131105d2f9de97fe
vaidyanathanms_cades <vm5@ornl.gov>
2026-08-25 14:47:59 -0400

    first version of all the codes. Need to be debugged

---
+++ src/src_py/extra_functions.py
@@ -0,0 +1,446 @@
+# Supporting files for genconf.py
+# Version: Feb-17-2026
+#------------------------------------------------------------------
+
+# Import modules
+import os
+import sys
+import numpy
+import re
+import shutil
+import glob
+import math
+import fileinput
+import subprocess
+from datetime import datetime 
+from pathlib import Path
+import fnmatch
+import pandas as pd
+from types import SimpleNamespace
+
+#------------------------------------------------------------------
+# General copy script
+def gencpy(dum_maindir,dum_destdir,fylname):
+
+    srcfyl = dum_maindir + '/' + fylname
+
+    if not os.path.exists(srcfyl):
+        print('ERROR: ', srcfyl, 'not found')
+        return
+
+    desfyl = dum_destdir + '/' + fylname
+    shutil.copy2(srcfyl,desfyl)
+    
+#------------------------------------------------------------------
+
+# Check for mdp files and copy/edit if not present
+def check_cpy_mdp_files(srcdir,destdir,mdp_fyles,resname_arr,Tetau_vr=0.1\
+                        ,Tetau_high_vr=0.1,Tetau_berend=0.1,Tetau_parrah = 0.2,\
+                        Prtau_berend=4.0,Prtau_crescale=4.0,Prtau_parrah=8.0,ref_temp=300,\
+                        hi_ref_temp=600,ref_pres=1.0):
+
+    # Only temperatures need to have multiple coupling groups
+    tc_grpdata      = " ".join(map(str, resname_arr))
+    lenresname      = len(resname_arr)
+    str_Tetau_vr    = " ".join([str(Tetau_vr)]*lenresname)
+    str_Tetau_hi_vr = " ".join([str(Tetau_high_vr)]*lenresname)
+    str_Tetau_ber   = " ".join([str(Tetau_berend)]*lenresname)
+    str_Tetau_par   = " ".join([str(Tetau_parrah)]*lenresname)
+    str_temp        = " ".join([str(ref_temp)]*lenresname)
+    str_hi_temp     = " ".join([str(hi_ref_temp)]*lenresname)
+
+
+    for mdp_fname in mdp_fyles:
+        if not os.path.exists(srcdir + '/' + mdp_fname):
+            raise RuntimeError(mdp_fname," not found in ",srcdir)
+        gencpy(srcdir,destdir,mdp_fname)
+        py_fname = mdp_fname
+        rev_fname = mdp_fname.replace('_pyinp','')
+        fr  = open(destdir + '/' + py_fname,'r')
+        fw  = open(destdir + '/' + rev_fname,'w')
+        fid = fr.read().replace("py_tcgrps",tc_grpdata).\
+            replace("py_Temptau_vr",str_Tetau_vr).\
+            replace("py_HighTemptau_vr",str_Tetau_hi_vr).\
+            replace("py_Temptau_Berend", str_Tetau_ber).\
+            replace("py_Temptau_ParRah",str_Tetau_par).\
+            replace("py_Prestau_Berend",str(Prtau_berend)).\
+            replace("py_Prestau_Crescale",str(Prtau_crescale)).\
+            replace("py_Prestau_ParRah",str(Prtau_parrah)).\
+            replace("py_ref_t",str_temp).\
+            replace("py_Highref_t",str_hi_temp).\
+            replace("py_ref_p",str(ref_pres))
+        fw.write(fid)
+        fw.close()
+        fr.close()
+        
+#------------------------------------------------------------------
+
+# Convert number from d.f to dpf
+def convert_number(x):
+    x = float(x)
+    s = format(x,'g')
+
+    if '.' not in s:
+        s += '.0'
+
+    return s.replace('.', 'p')
+
+#------------------------------------------------------------------
+
+# Match files without case sensitivity
+def glob_ci(root, wildcard_pattern):
+    wp = wildcard_pattern.casefold()
+    root = Path(root)
+    return [p for p in root.glob("*")
+            if p.is_file() and fnmatch.fnmatch(p.name.casefold(), wp)]
+
+#------------------------------------------------------------------
+
+# Compute total number of electrolyte atoms
+def compute_solv_molecules(s1name,s2name,v1frac):
+    chemicals = load_components("all_props.xlsx","PropertyData")
+    s1nmol = 100*v1frac
+    s1type = chemicals[s1name]; s2type = chemicals[s2name]
+    s1dens = s1name.density; s2dens = s2name.density
+    s1MW = s1name.MW; s2MW = s2name.MW
+    s2nmol = s2name.PureSolvNMols if v1frac == 0 \
+        else  round((s1nmol)*((100-v1frac)/(v1frac))*(s2dens/s1dens)*(s1MW/s2MW))
+    volsol = s1nmol*(s1MW/s1dens) + s2nmol*(s2MW/s2dens)
+    return s1nmol, s2nmol, volsol
+
+#------------------------------------------------------------------
+
+# Compute total number of electrolyte atoms
+def compute_elec_molecules(solvol,conc,ename):
+    chemicals = load_components("all_props.xlsx","PropertyData")
+    etype = chemicals[ename]
+    return solvol*conc*0.001 # For conversion to mol/cc
+
+#------------------------------------------------------------------
+
+# Compute total number of atoms and simulation box dimensions
+def compute_sim_dims(s1name,s1nmol,s2name,s2nmol,e1name,e1nmol,\
+                     e2name,e2nmol,init_dens = 0.8):
+    chemicals = load_components("all_props.xlsx","PropertyData")
+    s1type = chemicals[s1name]; s2type = chemicals[s2name]
+    e1type = chemicals[e1name]; e2type = chemicals[e2name]
+    totatoms = s1nmol*s1type.Natoms + s2nmol*s2type.Natoms + \
+        e1nmol*e1type.Natoms + e2nmol*e2type.Natoms
+    totmass = s1nmol*s1type.MW + s2nmol*s2type.MW + \
+        e1nmol*e1type.MW + e2nmol*e2type.MW
+    boxvol = totmass/0.6023 # conversion to Ang^3
+    return totatoms,totmass,boxvol,boxvol**(1.0/3.0)
+
+#------------------------------------------------------------------
+
+# Split cation and anion names and their valencies
+def split_ename(ename,enmol,cation_types,anion_types,valence_tuple):
+    
+    catname = next((word for word in cation_types if word in ename), None)
+    if catname is None:
+        raise ValueError(f"No matching cation type found in {ename}.")
+    anname  = next((word for word in anion_types if word in ename), None)
+    if catname is None:
+        raise ValueError(f"No matching cation type found in {ename}.")
+    catmol = enmol*int(valence_tuple[0])
+    anmol  = enmol*int(valence_tuple[1])
+    
+    return catmol,catname,anmol,anname
+
+# Generate arrays for writing GMX files
+def generate_gmx_arrs(s1name,s1nmol,s2name,s2nmol,e1name,e1nmol,\
+                      e2name,e2nmol):
+
+    itp_arr = []; cfg_arr = []; resname_arr = []
+    molname_arr = []; molval_arr = []
+    
+    if s1nmol != 0:
+        itp_arr.extend([s1name + '.itp'])
+        cfg_arr.extend([s1name + '.pdb'])
+        resname_arr.extend([s1resname])
+        molname_arr.extend([s1name])
+        molval_arr.extend([s1nmol])
+    if s2nmol != 0:
+        itp_arr.extend([s2name + '.itp'])
+        cfg_arr.extend([s2name + '.pdb'])
+        resname_arr.extend([s2resname])
+        molname_arr.extend([s2name])
+        molval_arr.extend([s2nmol])
+    if e1nmol != 0:
+        itp_arr.extend([catname + '.itp',anname+'.itp'])
+        cfg_arr.extend([catname + '.pdb',anname+'.pdb'])
+        resname_arr.extend([catresname,anresname])
+        molname_arr.extend([catname,anname])
+        molval_arr.extend([catmol,anmol])      
+    if e2nmol != 0:
+        itp_arr.extend([catname + '.itp',anname+'.itp'])
+        cfg_arr.extend([catname + '.pdb',anname+'.pdb'])
+        resname_arr.extend([catresname,anresname])
+        molname_arr.extend([catname,anname])
+        molval_arr.extend([catmol,anmol])      
+
+        return itp_arr,cfg_arr,resname_arr,molname_arr,molval_arr
+    
+#------------------------------------------------------------------
+
+# Copy coordinate files
+def cpy_coord_files(cfg_dir,inp_arr,workdir,destdirname="all_coords"):
+    destdir = workdir + '/' + destdirname
+    if not os.path.isdir(destdir):
+        os.mkdir(destdir)
+
+    coordfyle_list = [] 
+    for fname in inp_arr:
+        allfyles     = glob_ci(cfg_dir,fname+".pdb")
+        if allfyles == []:
+            raise RuntimeError(f"No coordinate file for {fname}")
+        else:
+            cname = max(allfyles,key=os.path.getmtime)
+            shutil.copy2(cname,destdir)
+            coordfyle_list.append(Path(cname).name)
+    return coordfyle_list
+
+#------------------------------------------------------------------
+
+# Copy itp files
+def cpy_itp_files(itp_dir,inp_arr,workdir,destdirname="itp_files"):
+    destdir = workdir + "/" + destdirname
+    extensions = [".itp",".btp",".prm"]
+    itp_filelist = []
+    if not os.path.isdir(destdir):
+        os.mkdir(destdir)
+    for fname in inp_arr:
+        allfyles = []
+        for ext in extensions:
+            allfyles.extend(glob_ci(itp_dir,fname+ext))
+        if allfyles == []:
+            raise RuntimeError(f"No itp/prm for {fname}")
+        for fyles in allfyles:
+            shutil.copy2(Path(fyles),destdir)
+            itp_filelist.append(Path(fyles).name)
+    return itp_filelist
+
+#------------------------------------------------------------------
+
+# reorder files with atom types followed by btp and itp files
+def reorder_itp_fnames(files):
+    # define desired order
+    order = {'.atp': 0, '.btp': 1, '.itp': 2, '.prm':3}
+    sorted_files = sorted(files, key=lambda x:\
+                          order.get(os.path.splitext(x)[1], 999))
+    return sorted_files
+
+#------------------------------------------------------------------   
+
+# Generate topology files            
+def gen_top_file(ff_indir,itp_fnames,molname_arr,molval_arr,workdir,\
+                 destdirname = "itp_files",top_name="topol.top",\
+                 nbfunc=1,comb_rule=2,gen_pairs='yes',\
+                 fLJ=0.5,fQQ=0.5,sysname="system"):
+    
+    with open(workdir + "/" + top_name,'w') as ftop:
+        now = datetime.now()
+        ftop.write('; topology file generated by VMS\n')
+        ftop.write('; ' + now.strftime("%B-%d-%Y, %I:%M:%S %p\n"))
+        ftop.write('\n')
+
+        ftop.write('[ defaults ]\n')
+        ftop.write('; nbfunc\tcomb-rule\tgen-pairs\tfudgeLJ\tfudgeQQ\n')
+        ftop.write(f'{nbfunc}\t{comb_rule}\t{gen_pairs}\t{fLJ}\t{fQQ}\n')
+
+        ftop.write('\n')
+        # add atomtypes and parameters before [ moleculetype ]
+        inc_top = ''
+        inc_pre = '#include '
+        for i in range(len(itp_fnames)):
+            ftop.write(inc_top + inc_pre +  '"./' + destdirname +\
+                       "/" + itp_fnames[i] + '"\n')
+
+        # Add system name
+        ftop.write('\n')
+        ftop.write('[ system ]\n')
+        ftop.write(f'{sysname}\n\n')
+
+        # Add molecule names and values
+        ftop.write('[ molecules ]\n')
+        for i in range(len(molname_arr)):
+            ftop.write(f'{molname_arr[i]}\t{int(molval_arr[i])}\n')
+
+#------------------------------------------------------------------
+
+# Set up packmol files
+def setup_packmol(inpfnames,molval_arr,workdir,boxl,\
+                  destdirname="all_coords",outname='mixture.pdb',\
+                  packname='make_mixture.inp'):
+
+    with open(workdir + '/' + packname,'w') as fpack:
+        fpack.write('# \n # A mixture of ionic liquids & diluents or pure ionic liquids\n')
+        fpack.write('# Min separation: 2 Ang\n')
+        fpack.write('# input filetype: pdb\n')
+
+        fpack.write(f'tolerance\t2.0\n')
+        fpack.write(f'filetype\tpdb\n')
+        fpack.write(f'output\t{outname}\n\n')
+
+        # Add each molecule with their type
+        for i,fname in enumerate(inpfnames):
+            molname = fname.split(".")[0]
+            fpack.write(f'# Add {molname}\n')
+            fpack.write(f'structure\t ./{destdirname}/{fname}\n')
+            fpack.write(f'number\t{str(int(molval_arr[i]))}\n')
+            fpack.write(f'inside box 0. 0. 0. {boxl} {boxl} {boxl}\n')
+            fpack.write(f'end structure\n\n')
+
+#------------------------------------------------------------------
+
+def make_anainps(workdir,saltcatname,orgcatname,resnames,\
+                 rdftuple=list(),rdfcalc=1,diffcalc=1,\
+                 autocorrcalc=1):
+    if rdfcalc:
+        if not os.path.isdir(workdir + '/anainps'):
+            os.mkdir(workdir + '/anainps')
+
+
+        with open(workdir + '/anainps/ref_all.txt','w') as frdf:
+            for res in range(len(resnames)-1):
+                frdf.write(f'resname {resnames[res]};\n')
+            frdf.write(f'resname {resnames[-1]}')
+            
+            if rdftuple:
+                for res,typ in rdftuple:
+                    frdf.write(f';\nresname {res} and type {typ}')
+
+            rdfsaltcat = '/anainps/ref'+saltcatname+'.txt'
+            with open(workdir + rdfsaltcat,'w') as fs:
+                fs.write(f'resname {saltcatname}')
+
+            rdforgcat = '/anainps/ref'+orgcatname+'.txt'
+            with open(workdir + rdforgcat,'w') as fs:
+                fs.write(f'resname {orgcatname}')
+
+#------------------------------------------------------------------
+
+# Copy and run shell script files
+def cpy_anash_files(srcdir,destdir,sh_ana_fyle,saltcat,\
+                    saltan,orgcat,organ,diluent,begtime,endtime,\
+                    rdfcalc=1,diffcalc=1,runall=0,jname="default"):
+
+    gencpy(srcdir,destdir,sh_ana_fyle)
+    py_fname = destdir + '/' + sh_ana_fyle
+    rev_fname = py_fname.replace('_pyinp','')
+    fr  = open(py_fname,'r')
+    fw  = open(rev_fname,'w')
+    fid = fr.read().replace("py_jobname","ana_"+jname).\
+        replace("py_saltcation",saltcat).\
+        replace("py_saltanion",saltan).\
+        replace("py_orgcation",orgcat).\
+        replace("py_organion",organ).\
+        replace("py_diluent",diluent).\
+        replace("py_begtime",str(begtime)).\
+        replace("py_endtime",str(endtime)).\
+        replace("py_rdfcalc",str(rdfcalc)).\
+        replace("py_difcalc",str(diffcalc))
+    fw.write(fid)
+    fr.close(); fw.close()
+
+    if runall:
+        os.chdir(destdir)
+        print(f"Submitting {rev_fname} ..")
+        subprocess.call(["chmod", "777", rev_fname])
+        subprocess.call(["sbatch", rev_fname])
+#------------------------------------------------------------------
+
+# Copy and run shell script files
+def cpy_sh_files(srcdir,destdir,sh_pp_fyle,sh_md_fyle,boxl,\
+                 runall=0,outname='mixture.pdb',\
+                 packname='make_mixture.inp',top_fyle="topol.top",\
+                 jname="default",qname='-A chem',pname='burst',\
+                 hpcname='cades'):
+
+    # Check for tpr files to provide run conditions
+    if glob.glob(destdir+'/*.tpr') == []:
+        print('No tpr files found. Beginning new runs..')
+        if not os.path.exists(srcdir+'/' + sh_pp_fyle):
+            raise RuntimeError(sh_pp_fyle+" not found in "+\
+                               srcdir)
+        gencpy(srcdir,destdir,sh_pp_fyle)
+
+        py_fname = destdir + '/' + sh_pp_fyle
+        rev_fname = py_fname.replace('_pyinp','')
+        fr  = open(py_fname,'r')
+        fw  = open(rev_fname,'w')
+        fid = fr.read().replace("py_qname",qname).\
+            replace("py_hpcname",hpcname).\
+            replace("py_pname",pname).\
+            replace("py_jobname","pp_"+jname).\
+            replace("py_packname",packname).\
+            replace("py_confname",outname).\
+            replace("py_boxl",str(boxl+0.2)).\
+            replace("py_topname",top_fyle)
+        fw.write(fid)
+        fr.close(); fw.close()
+        
+    else:
+        print('tpr files found. Continuing runs..')
+        if not os.path.exists(srcdir+'/' + sh_md_fyle):
+            raise RuntimeError(sh_md_fyle+" not found in "+\
+                               srcdir)
+        gencpy(srcdir,destdir,sh_md_fyle)
+
+        py_fname = destdir + '/' + sh_md_fyle
+        rev_fname = py_fname.replace('_pyinp','')
+        fr  = open(py_fname,'r')
+        fw  = open(rev_fname,'w')
+        fid = fr.read().replace("py_qname",qname).\
+            replace("py_hpcname",hpcname).\
+            replace("py_pname",pname).\
+            replace("py_jobname",'md_'+jname).\
+            replace("py_topol",top_fyle)
+        fw.write(fid)
+        fr.close(); fw.close()
+
+        
+    if runall:
+        os.chdir(destdir)
+        print(f"Submitting {rev_fname} ..")
+        subprocess.call(["chmod", "777", rev_fname])
+        subprocess.call(["sbatch", rev_fname])
+#------------------------------------------------------------------
+
+def clean_up(workdir):
+    all_files = glob.glob(workdir + '/*_pyinp*')
+    if not all_files == []:
+        for fyle in all_files:
+            os.remove(fyle)
+#------------------------------------------------------------------
+
+def load_components(excel_file, sheet_name='MolData'):
+    df = pd.read_excel(excel_file, sheet_name=sheet_name)
+
+    df = df.rename(columns={
+        "Molecule name": "molname",
+        "Abbreviation": "abbreviation",
+        "# of atoms": "Natoms",
+        "MW (g/mol)": "MW",
+        "density (g/cc)": "density",
+        "Pure_Solv_#_molecules": "PureSolvNMols",
+    })
+
+    df = df.dropna(subset=["abbreviation"])
+
+    components = {}
+
+    for _, row in df.iterrows():
+        abbreviation = str(row["abbreviation"]).strip()
+
+        components[abbreviation] = SimpleNamespace(
+            atoms=int(row["Natoms"]),
+            MW=float(row["MW"]),
+            density=float(row["density"]),
+            PureNMols=float(row["PureSolvNMols"]),
+        )
+
+        setattr(components, abbreviation, component)
+
+    return components
+#------------------------------------------------------------------
---
+++ src/src_py/genconf.py
@@ -0,0 +1,215 @@
+# Generate configurations for mixed-solvent simulations
+# Author: Vaidyanathan M. S
+
+# Version: Aug-24-2026
+#------------------------------------------------------------------
+#Import modules 
+import os
+import sys
+import numpy as np
+import re
+import shutil
+import glob
+import math
+import subprocess
+import extra_functions as ef
+import directories
+#------------------------------------------------------------------ 
+# Version Info
+print("Generating GROMACS run-time inputs")
+print("Version: Aug-18-2026")
+
+#------------------------------------------------------------------
+# Input data
+# Note: 100% water is kept at 99.99% to avoid NaN errors
+run_all  = 0 # 1-copy files and run, 0-NO run (copies files)
+
+cation_types      = ['Fe','Na']
+anion_types       = ['SO4','Cl']
+electrolyte_1_arr = ['FeSO4','FeCl']
+valence_1_arr     = [(1,1),(1,2)]
+electrolyte_2_arr = ['NaSO4','NaCl']
+valence_2_arr     = [(2,1),(1,1)]
+
+solvent_1_arr     = ['WAT']
+solvent_2_arr     = ['EGL','MET','TGL','THF','ACN']
+sol1_volfracs_arr = np.array([100,90,75,50,25,0]) # in %
+sol1_conc_arr     = np.array([0.1, 1]) # in mol/l
+sol2_conc_arr     = np.array([0, 0.05]) # in mol/l
+
+#------------------------------------------------------------------
+# Required GMX/sh and default gro/top files
+mdp_fyles  = ['minim_pyinp.mdp','nvt_pyinp.mdp',\
+              'npt_crescale_pyinp.mdp','npt_main_pyinp.mdp']
+sh_md_fyle = 'run_md_pyinp.sh'
+sh_pp_fyle = 'run_preprocess_pyinp.sh'
+sh_rep_fyl = ['repeat_all.sh','repeat_md.sh']
+def_inicon = 'initconf.gro'
+
+#------------------------------------------------------------------
+# Simulation details
+init_density      = 0.8 # in g/cc
+
+#------------------------------------------------------------------
+# Check directories
+curr_dir = os.getcwd()
+if not os.path.isdir(main_dir):
+    raise RuntimeError('Check path to src files and update main_dir')
+if not os.path.isdir(scr_dir):
+    raise RuntimeError('Check path to working dir and update scr_dir')
+
+#------------------------------------------------------------------
+# Create head scratch directory
+scr_maindir = scr_dir + '/mix_solv_systems'
+if not os.path.isdir(scr_maindir):
+    os.mkdir(scr_maindir)
+    
+#------------------------------------------------------------------
+#------------------Main-loop starts here---------------------------
+# Loop over solvent-1
+for s1id,s1name in enumerate(solvent_1_arr):
+                        
+    # Loop over solvent-2
+    for s2id,s2name in enumerate(solvent_2_arr):
+        
+        # Loop over volume fractions of solvent-1
+        for v1id,v1frac in enumerate(sol1_volfracs_arr):
+            # Compute required number of molecules of each solvent
+            s1nmol,s2nmol,volsol = ef.compute_solv_molecules(s1name,\
+                                                             s2name,\
+                                                             v1frac)
+
+            # Loop over concentrations of solvent-1
+            for c1id,c1val in enumerate(sol1_conc_arr):
+
+                # Loop over concentrations of solvent-2
+                for c2id,c2val in enumerate(sol2_conc_arr):
+
+                    # Loop over electrolyte-1
+                    for e1id,e1name in enumerate(electrolyte_1_arr):
+                        # Split cations and anions in Elec-1
+                        catmol,catname,anmol,anname = \
+                            ef.split_ename(e1name,e1nmol,\
+                                           cation_types,\
+                                           anion_types,\
+                                           valence_1_arr[e1id])
+
+                        # Compute number of molecules of electrolyte-1
+                        e1nmol = ef.compute_elec_molecules(volsol,\
+                                                           c1val,\
+                                                           e1name)
+                        # Loop over electrolyte-2
+                        for e2id,e2name in enumerate(electrolyte_2_arr):
+                            # Split cations and anions in Elec-1
+                            catmol,catname,anmol,anname = \
+                                ef.split_ename(e2name,e2nmol,\
+                                               cation_types,\
+                                               anion_types,\
+                                               valence_2_arr[e2id])
+
+                            # Compute number of molecules of electrolyte-2
+                            e2nmol = ef.compute_elec_molecules(volsol,\
+                                                               c2val,\
+                                                               e2name)
+                            elec1dir = scr_dir + '/' + e1name + \
+                                '_conc_' + str(c1val)
+                            if not os.path.isdir(elec1dir):
+                                os.mkdir(elecdir)
+
+                            elec2dir = elec1dir + '/' + e2name + \
+                                '_conc_' + str(c2val)
+                            if not os.path.isdir(elec2dir):
+                                os.mkdir(elec22dir)
+
+                            solvdir = elec2dir + '/' + s1name + '_' s2name
+                            if not os.path.isdir(solvdir):
+                                os.mkdir(solvdir)
+
+                            workdir = solvdir + '/volfrac_' + str(v1frac)
+                            if not os.path.isdir(workdir):
+                                os.mkdir(solvdir)
+                            
+                            print(f'Setting up simulations for
+                            Solvent-1: {s1name}; Solvent-2: {s2name};
+                            Elec-1: {e1name}; Elec-2: {e2name}')  
+                                
+                            # Set-up box dimensions 
+                            totnatoms,totmass,boxvol,boxlen = \
+                                ef.compute_sim_dims(s1name,s1nmol,\
+                                                    s2name,s2nmol,\
+                                                    e1name,e1nmol,\
+                                                    e2name,e2nmol,\
+                                                    init_dens = init_density)
+                            
+
+                            # Set-up gmx inputs
+                            itp_arr,cfg_arr,resname_arr,molname_arr,molval_arr = \
+                                ef.generate_gmx_arrs(s1name,s1nmol,s2name,s2nmol,\
+                                                     e1name,e1nmol,e2name,e2nmol)
+
+                            sysname = f"{e1name}_{c1val:.1f}_{e2name}_{c2val:.1f}_{s1name}_{volfrac.2f}_{s2name}"
+
+                            
+                            # Copy and edit mdp files
+                            print('Copying and editing mdp files ...')
+                            ef.check_cpy_mdp_files(mdp_dir,workdir,mdp_fyles,\
+                                                   resname_arr,Tetau_vr=0.1,\
+                                                   Tetau_high_vr=0.1,\
+                                                   Tetau_berend=0.1,\
+                                                   Tetau_parrah = 0.2,\
+                                                   Prtau_berend=4.0,\
+                                                   Prtau_crescale=4.0,\
+                                                   Prtau_parrah=8.0,\
+                                                   ref_temp=300,hi_ref_temp=600,\
+                                                   ref_pres=1.0)
+
+                            # Copy coordinate files
+                            print('Copying coordinate files ...')
+                            coord_fnames = \
+                                ef.cpy_coord_files(cfg_dir, cfg_arr, workdir,\
+                                                   destdirname="all_coords")
+
+
+                            # Copy and edit itp files
+                            print('Copying itp or prm files ...')
+                            itp_fnames = ef.cpy_itp_files(itp_dir,\
+                                                          itp_arr,workdir,\
+                                                          destdirname="itp_files")
+                            itp_fnames = ef.reorder_itp_fnames(itp_fnames)
+                            
+                            # Copy atype files
+                            ef.gencpy(itp_dir,workdir+'/itp_files',attype_fname) 
+                            itp_fnames.insert(0,attype_fname)
+
+                            # Generate top files
+                            print('Generating top file ...')
+                            ef.gen_top_file(itp_dir,itp_fnames,molname_arr,\
+                                            molval_arr,workdir,\
+                                            destdirname="itp_files",\
+                                            top_name="topol.top",\
+                                            nbfunc=1,comb_rule=3,gen_pairs='yes',\
+                                            fLJ=0.5,fQQ=0.5,sysname=sysname)
+
+                            # Generating packmol files
+                            print('Generating packmol file ...')
+                            ef.setup_packmol(coord_fnames,molval_arr,workdir,\
+                                             box_arr_ang[iarr],\
+                                             destdirname="all_coords",\
+                                             outname='mixture.pdb',\
+                                             packname = 'make_mixture.inp')
+                            
+                            # Run files
+                            print('Generating shell script files ...')
+                            ef.cpy_sh_files(sh_dir,workdir,sh_pp_fyle,\
+                                            sh_md_fyle,box_arr_nm[iarr],\
+                                            runall=run_all,\
+                                            outname='mixture.pdb',\
+                                            packname='make_mixture.inp',\
+                                            top_fyle= "topol.top",\
+                                            jname=sysname,qname=qname,\
+                                            pname=pname,hpcname=hpcname)
+
+                            
+                            # Cleaning up
+                            print('Cleaning up directory ..')
+                            ef.clean_up(workdir)
